a636f9be75af2e5f52f53db8e66a77b963f2bc49
Farren S Curtis <FarrenSCurtis@Macintosh-148.local>
2015-06-03 14:13:05 -0500

    changed mutation prob

--- src/crossover/4mol_crossover.py
+++ src/crossover/4mol_crossover.py
@@ -184,6 +184,7 @@ class Crossover(object):
     def choose_crossover_type(self):
 	geom_opts = [1, 2, 3, 4, 5, 6, 7, 8]
         lat_opts = [1, 2, 3]
+	#2/24=1/12 chance of single parent crossover [1,1] or [2,2]
 	cross_types = [[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3],[4,1],[4,2],[4,3],[5,1],[5,2],[5,3],[6,1],[6,2],[6,3],[7,1],[7,2],[7,3],[8,1],[8,2],[8,3]]
 	method = random.choice(cross_types)
 	return method
