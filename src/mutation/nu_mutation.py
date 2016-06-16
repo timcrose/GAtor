f1f348bd7ee8f1b76c1eb9572981c590dc7c571f
Farren Curtis <fcurtis@cypress1.cm.cluster>
2016-06-16 10:20:45 -0500

    new mutation

---
+++ src/mutation/nu_mutation.py
@@ -0,0 +1,21 @@
+'''
+@author: farren
+'''
+from __future__ import division
+
+from copy import deepcopy
+import numpy as np
+import random
+import time
+
+from core import user_input,output
+from structures.structure import Structure
+
+def main(struct, replica):
+    '''
+    Args: Single Structure() to mutate, the replica name running the crossover instance.
+    Returns: A single Structure() if mutation is successful or False if mutation fails 
+    '''
+    input_struct = 
+   
+
