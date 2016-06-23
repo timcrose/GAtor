0c1f86969f25962fe53177516ebf1b7dff92099e
Xiayue Li <xli20@cypress1.cm.cluster>
2016-06-23 10:16:42 -0500

    Edits to increase get boolean and deepcopy

--- src/core/user_input.py
+++ src/core/user_input.py
@@ -12,27 +12,53 @@ from core.file_handler import default_config, ui_conf
 DEFAULT_CONFIG_REPLICA = -1
 
 class ListSafeConfigParser(SafeConfigParser):
-    '''Inherits SafeConfigParser and provides list parsing with json'''
-    
-    # TODO: maybe i could use literaleval(super.get()) instead, so to always return lists and ints
-    def get_list(self, section, option):
-        '''provides list parsing with json'''
-        return self.get(section, option).split()
-    
-    def get_eval(self, section, option):
-        return ast.literal_eval(self.get(section, option))
-    
+	'''Inherits SafeConfigParser and provides list parsing with json'''
+	
+	# TODO: maybe i could use literaleval(super.get()) instead, so to always return lists and ints
+	def get_list(self, section, option):
+		'''provides list parsing with json'''
+		return self.get(section, option).split()
+	
+	def get_eval(self, section, option):
+		return ast.literal_eval(self.get(section, option))
+
+	def get_boolean(self,section,option):
+		'''
+		Check and see if the section and option is specified
+		if specified, has to set to "TRUE", else an error will be raised
+		'''
+		if self.has_option(section,option):
+			if self.get(section,option)!="TRUE":
+				raise ValueError("Optional boolean flag has to be set to TRUE if present")
+			return True
+		return False
+
+	def __deepcopy__(self,memo):
+		'''
+		Due to the inability to deepcopy a configuration file
+		Will generate a temporary config file and read it back in
+		'''
+		config_string = StringIO()
+		self.write(config_string)
+		config_string.seek(0)
+		copied = ListSafeConfigParser()
+		copied.readfp(config_string)
+		return copied
+
+
+
+	
 def get_config():
-    '''
-    Reads in default and user defined UI from the filesystem
-    '''
-    config = ListSafeConfigParser()
+	'''
+	Reads in default and user defined UI from the filesystem
+	'''
+	config = ListSafeConfigParser()
 
-    default_config_file = open(default_config, 'r')
-    config.readfp(default_config_file)
-    default_config_file.close()
+	default_config_file = open(default_config, 'r')
+	config.readfp(default_config_file)
+	default_config_file.close()
 
-    local_config_file = open(ui_conf, 'r')
-    config.readfp(local_config_file)
-    local_config_file.close()
-    return config
+	local_config_file = open(ui_conf, 'r')
+	config.readfp(local_config_file)
+	local_config_file.close()
+	return config
