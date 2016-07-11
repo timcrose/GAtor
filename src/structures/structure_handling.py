a9d00b7fe21734d1f7fb69c720c2b1fe79155f91
Course Tutor <tutor@cips2.theo.chemie.tu-muenchen.de>
2016-07-11 20:36:23 +0200

    cosmetic fix so output is aligned

--- src/structures/structure_handling.py
+++ src/structures/structure_handling.py
@@ -480,7 +480,7 @@ def cell_modification (struct,replica=ui.get_replica_name(),create_duplicate=Tru
 	struct=cell_lower_triangular(struct,False)
 
 	if count>0 and verbose:
-		st="-- Cell modification required for structure " 
+		st="-- Cell modification required  for structure " 
 		if struct.struct_id!=None:
 			st += str(struct.struct_id)
 		else:
