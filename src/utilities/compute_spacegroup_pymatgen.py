d28feffa3b72a8dfd3a866a2f3d1f7acc514e180
Farren Curtis <fcurtis@tulane.edu>
2017-02-28 15:50:41 +0000

    pymtagen compatibility fix

--- src/utilities/compute_spacegroup_pymatgen.py
+++ src/utilities/compute_spacegroup_pymatgen.py
@@ -1,40 +1,47 @@
 '''
-Computes Spacegroup of Structure()
+Computes the spacegroup for the given structure
+using the python pacakge pymatgen
 
-Created by Farren Curtis on July5 5th, 2016
+Created by Farren Curtis on July 5th, 2016
 '''
 from pymatgen import Lattice as LatticeP
 from pymatgen import Structure as StructureP
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer as SGA
 
 def main(struct):
-	''' 
-        Args: Structure() from the GA
-        Returns: Structure with pymatgen spacegroup attached a property
+    ''' 
+    Args: Structure() from the GA
+    Returns: Structure with approx spacegroup attached a property
 	'''
 	structp = get_pymatgen_structure(struct.get_frac_data())
 	SG = return_spacegroup(structp)
 	struct.set_property("space_group",SG)
+	print struct.struct_id
 	return struct
 
 def get_pymatgen_structure(frac_data):
-        '''
-        Args: self, Geometric data from GAtor's Structure() object
-        Returns: A pymatgen StructureP() object with the same geometric properties
-        '''
-        coords = frac_data[0] # frac coordinates
-        atoms = frac_data[1] # site labels
-        lattice = (LatticeP.from_parameters(a=frac_data[2], b=frac_data[3], c=frac_data[4],
-                                  alpha=frac_data[5],beta=frac_data[6], gamma=frac_data[7]))
-        structp = StructureP(lattice, atoms, coords)
-        return structp
+    '''
+    Args: self, Geometric data from GAtor's Structure() object
+    Returns: A pymatgen StructureP() object with the same 
+    geometric properties
+    '''
+    coords = frac_data[0] # frac coordinates
+    atoms = frac_data[1] # site labels
+    lattice = (LatticeP.from_parameters(a=frac_data[2], 
+                                        b=frac_data[3], 
+                                        c=frac_data[4], 
+                                        alpha=frac_data[5],
+                                        beta=frac_data[6], 
+                                        gamma=frac_data[7]))
+    structp = StructureP(lattice, atoms, coords)
+    return structp
 
 def return_spacegroup(structp):
     try:
         SG = SGA(structp, symprec= 1.0).get_spacegroup_number()
-    except:
+    except: #different for newer versions of pymatgen
         SG = SGA(structp, symprec= 1.0).get_space_group_number()
     return SG
 
 if __name__ == "__main__":
-        main()
+    main()
