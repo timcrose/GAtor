d920b3b3f81ff80a461844ac74d552b64fed3510
Farren Curtis <fcurtis@tulane.edu>
2017-03-28 19:28:07 +0000

    space/tab issues

--- src/GAtor_master.py
+++ src/GAtor_master.py
@@ -24,117 +24,101 @@ def main():
 	main_process = GAtor()
 
 class GAtor():
-	'''
-	This is the master class of GAtorr
-	Takes the path to a configuration file as an necessary input
-	'''
-	def __init__(self):
-		sname = "GAtor_master"
-		self.ui = user_input.get_config()
-		if self.ui.has_option(sname,"working_directory"):
-			os.chdir(self.ui.get(sname,"working_directory"))
-			reload_modules()
-			
-		if self.ui.get_boolean(sname,"testing_mode"):
-			print("Testing mode starting")
-			self.testing_mode()
-			return #Under test mode, no further comamnd is read
+    '''
+    This is the master class of GAtorr
+    Takes the path to a configuration file as an necessary input
+    '''
+    def __init__(self):
+        sname = "GAtor_master"
+        self.ui = user_input.get_config()
+        if self.ui.has_option(sname,"working_directory"):
+            os.chdir(self.ui.get(sname,"working_directory"))
+            reload_modules()
+        if self.ui.get_boolean(sname,"testing_mode"):
+            self.testing_mode()
+            return #Under test mode, no further comamnd is read
+        if self.ui.get_boolean(sname,"bundled_ga"):
+            parallel_run.launch_bundled()
+            return
+        if self.ui.get_boolean(sname,"kill_ga") and\
+            self.ui.is_master_process():
+                self.kill_GA()
+        if self.ui.get_boolean(sname,"clean_folder") and\
+            self.ui.is_master_process():
+                self.clean_folder()
+        if self.ui.get_boolean(sname,"fill_initial_pool") and\
+            self.ui.is_master_process():
+			    self.fill_initial_pool()
+        if self.ui.get_boolean(sname,"run_ga"):
+            self.run_ga()
 
-		if self.ui.get_boolean(sname,"bundled_ga"):
-			parallel_run.launch_bundled()
-			return
+    def testing_mode(self):
+        from utilities import test_and_debug
+        test_procedure = self.ui.get("test_and_debug","testing_procedure")
+        if self.ui.is_master_process():
+            output.time_log("Testing and debugging mode enabled")
+            output.time_log("Testing procedure used: "+test_procedure)
+        getattr(test_and_debug,test_procedure)()
+        return
 
-		if self.ui.get_boolean(sname,"kill_ga") and\
-					self.ui.is_master_process():
-			self.kill_GA()
+    def kill_GA(self):
+        kill.set_kill()
+        return
+    
+    def fill_initial_pool(self):
+        IP_module = fh.my_import(self.ui.get("modules","initial_pool_module"),package="initial_pool")
+        fh.mkdir_p(fh.out_tmp_dir)
+        IP_module.main()
 
-		if self.ui.get_boolean(sname,"clean_folder") and\
-					self.ui.is_master_process():
-			self.clean_folder()
-
-		if self.ui.get_boolean(sname,"fill_initial_pool") and\
-					self.ui.is_master_process():
-			self.fill_initial_pool()
-
-		if self.ui.get_boolean(sname,"run_ga"):
-			self.run_ga()		
-
-	def testing_mode(self):
-		from utilities import test_and_debug
-		test_procedure = self.ui.get("test_and_debug","testing_procedure")
-		if self.ui.is_master_process():
-			output.time_log("Testing and debugging mode enabled")
-			output.time_log("Testing procedure used: "+test_procedure)
-		
-		getattr(test_and_debug,test_procedure)()
-		return
-
-	def kill_GA(self):
-		kill.set_kill()
-		return
-
-	def fill_initial_pool(self):
-		IP_module = fh.my_import(self.ui.get("modules","initial_pool_module"),package="initial_pool")
-		fh.mkdir_p(fh.out_tmp_dir)
-		IP_module.main()
-		
-
-	def clean_folder(self):
-		sname = "clean_folder"
-		output.time_log("Cleaning folder is activated; pending user confirmation", sname)
-		user_answer = user_input.keyboard_input("Are you sure you want to clean the working directory (y/n):",
+    def clean_folder(self):
+        sname = "clean_folder"
+        output.time_log("Cleaning folder is activated; pending user confirmation", sname)
+        user_answer = user_input.keyboard_input("Are you sure you want to clean the working directory (y/n):",
                                                 allowed_answers=["y","n"],time_out=30)
-		if user_answer=="y":
-			output.time_log("User confirmation received. Begins cleaning.",sname)
-			clean()
-		else:
-			output.time_log("User denied cleaning",sname)
-			output.time_log("Moving on",sname)
+        if user_answer=="y":
+            output.time_log("User confirmation received. Begins cleaning.",sname)
+            clean()
+        else:
+            output.time_log("User denied cleaning",sname)
+            output.time_log("Moving on",sname)
 
-	def run_ga(self):
-		from core import run_GA
-		sname = "parallel_settings"
+    def run_ga(self):
+        from core import run_GA
+        sname = "parallel_settings"
         time.sleep(20)
-		if self.ui.get(sname,"parallelization_method") != "serial":
-		#Launch parallelism
-			parallel_run.launch_parallel()
-			return
-
-		if self.ui.get_replica_name() == "master":
-			#Need to assign new name
-			self.ui.set(sname,"replica_name",misc.get_random_index())
-			conf_path = os.path.join(fh.conf_tmp_dir,
-						 self.ui.get_replica_name()+".conf")
-
-			f = open(conf_path,"w")
-			self.ui.write(f)
-			f.close()
-			sys.argv.append(conf_path)
-			reload_modules()
-
-		message = "GAtor instance reporting from " + \
-				socket.gethostname()
-		if self.ui.has_option(sname,"allocated_nodes"):
-			nodes = self.ui.get_eval(sname,"allocated_nodes")
-			message += "; controlling node(s): " + \
+        if self.ui.get(sname,"parallelization_method") != "serial":
+            #Launch parallelism
+            parallel_run.launch_parallel()
+            return
+        if self.ui.get_replica_name() == "master":
+            #Need to assign new name
+            self.ui.set(sname,"replica_name",misc.get_random_index())
+            conf_path = os.path.join(fh.conf_tmp_dir,
+                        self.ui.get_replica_name()+".conf")
+            f = open(conf_path,"w")
+            self.ui.write(f)
+            f.close()
+            sys.argv.append(conf_path)
+            reload_modules()
+        message = "GAtor instance reporting from " + \
+                                socket.gethostname()
+        if self.ui.has_option(sname,"allocated_nodes"):
+            nodes = self.ui.get_eval(sname,"allocated_nodes")
+            message += "; controlling node(s): " + \
 					", ".join(map(str,nodes))
+        if self.ui.has_option(sname,"processes_per_replica"):
+            ppr = self.ui.get_eval(sname, "processes_per_replica")
+            message += "; controlling %i process(es)" % ppr
+        if self.ui.has_option(sname,"runjob_block"):
+            message += "; block: " + self.ui.get(sname,"runjob_block")
+        if self.ui.has_option(sname,"runjob_corner"):
+            message += "; corner: " + self.ui.get(sname,"runjob_corner")
 
-		if self.ui.has_option(sname,"processes_per_replica"):
-			ppr = self.ui.get_eval(sname, "processes_per_replica")
-			message += "; controlling %i process(es)" % ppr
-
-		if self.ui.has_option(sname,"runjob_block"):
-			message += "; block: " + self.ui.get(sname,"runjob_block")
-
-		if self.ui.has_option(sname,"runjob_corner"):
-			message += "; corner: " + self.ui.get(sname,"runjob_corner")
-
-		output.time_log(message)
-	
-		stoic = stoic_model.determine_stoic()
-		ga = run_GA.RunGA(self.ui.get_replica_name(),stoic)
-		ga.start()
-		output.move_to_shared_output(self.ui.get_replica_name())
+        output.time_log(message)
+        stoic = stoic_model.determine_stoic()
+        ga = run_GA.RunGA(self.ui.get_replica_name(),stoic)
+        ga.start()
+        output.move_to_shared_output(self.ui.get_replica_name())
 	
 
 
