e1597b735858ed2f2f3fca9cabc4b705beee6739
fcurtis <farrencurtis8@gmail.com>
2018-03-12 22:09:26 -0600

    Adds header

--- src/initial_pool/IP_filling.py
+++ src/initial_pool/IP_filling.py
@@ -1,6 +1,12 @@
-'''
-@author: farren 
-'''
+"""                                                                            
+If any part of this module is used for a publication please cite:              
+                                                                               
+F. Curtis, X. Li, T. Rose, A. Vazquez-Mayagoitia, S. Bhattacharya,             
+L. M. Ghiringhelli, and N. Marom "GAtor: A First-Principles Genetic            
+Algorithm for Molecular Crystal Structure Prediction",                         
+J. Chem. Theory Comput., DOI: 10.1021/acs.jctc.7b01152;                        
+arXiv 1802.08602 (2018)                                                        
+""" 
 from __future__ import division
 import os
 import itertools
@@ -15,8 +21,19 @@ from structures.structure_collection import StructureCollection
 from utilities import compute_spacegroup_pymatgen
 from pymatgen import Lattice as LatticeP
 from pymatgen import Structure as StructureP
-from pymatgen.analysis.structure_matcher import StructureMatcher,ElementComparator,SpeciesComparator,FrameworkComparator
-
+from pymatgen.analysis.structure_matcher import StructureMatcher,ElementComparator,\
+                                                SpeciesComparator,FrameworkComparator
+__author__ = "Farren Curtis, Xiayue Li, and Timothy Rose"                      
+__copyright__ = "Copyright 2018, Carnegie Mellon University and "+\
+                "Fritz-Haber-Institut der Max-Planck-Gessellschaft"            
+__credits__ = ["Farren Curtis", "Xiayue Li", "Timothy Rose",                   
+               "Alvaro Vazquez-Mayagoita", "Saswata Bhattacharya",             
+               "Luca M. Ghiringhelli", "Noa Marom"]                            
+__license__ = "BSD-3"                                                          
+__version__ = "1.0"                                                            
+__maintainer__ = "Timothy Rose"                                                
+__email__ = "trose@andrew.cmu.edu"                                             
+__url__ = "http://www.noamarom.com"  
 
 def main():
     '''
