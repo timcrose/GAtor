4231757121fb149b915c03851d56adf438f1d406
fcurtis <fcurtis@andrew.cmu.edu>
2018-04-03 14:16:31 +0000

    adds header

--- src/utilities/parallel_run.py
+++ src/utilities/parallel_run.py
@@ -1,8 +1,15 @@
-'''
+"""
 Handles parallelization of the GA
 
-Created by Patrick Kilecdi on June 26th, 2016
-'''
+If any part of this module is used for a publication please cite:              
+                                                                               
+F. Curtis, X. Li, T. Rose, A. Vazquez-Mayagoitia, S. Bhattacharya,             
+L. M. Ghiringhelli, and N. Marom "GAtor: A First-Principles Genetic            
+Algorithm for Molecular Crystal Structure Prediction",                         
+J. Chem. Theory Comput., DOI: 10.1021/acs.jctc.7b01152;                        
+arXiv 1802.08602 (2018)                                                        
+"""
+
 import subprocess, os, math
 import core.file_handler as fh
 from core import user_input, output
@@ -13,6 +20,19 @@ from external_libs import bgqtools
 from external_libs.filelock import FileLock
 import time
 import multiprocessing
+
+__author__ = "Farren Curtis, Xiayue Li, and Timothy Rose"
+__copyright__ = "Copyright 2018, Carnegie Mellon University and "+\
+                "Fritz-Haber-Institut der Max-Planck-Gessellschaft"
+__credits__ = ["Farren Curtis", "Xiayue Li", "Timothy Rose",
+               "Alvaro Vazquez-Mayagoita", "Saswata Bhattacharya",
+               "Luca M. Ghiringhelli", "Noa Marom"]
+__license__ = "BSD-3"
+__version__ = "1.0"
+__maintainer__ = "Timothy Rose"
+__email__ = "trose@andrew.cmu.edu"
+__url__ = "http://www.noamarom.com"
+
 ui = user_input.get_config()
 
 def launch_parallel():
