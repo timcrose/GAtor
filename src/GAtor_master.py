75f8c77c2e95727300b9d3d969c04e18b6282ac7
fcurtis <fcurtis@andrew.cmu.edu>
2018-04-23 14:02:31 +0000

    Initial commit using MPI4py

--- src/GAtor_master.py
+++ src/GAtor_master.py
@@ -15,10 +15,11 @@ arXiv 1802.08602 (2018)
 import os, subprocess, shutil
 import sys,socket
 import time
+import imp
 import core.file_handler as fh
 from core import user_input, output, check_conf
-from utilities import parallel_run, stoic_model, misc
-import imp
+from utilities import stoic_model, misc
+
 
 __author__ = "Farren Curtis, Xiayue Li, and Timothy Rose"
 __copyright__ = "Copyright 2018, Carnegie Mellon University and "+\
@@ -33,10 +34,13 @@ __email__ = "trose@andrew.cmu.edu"
 __url__ = "http://www.noamarom.com"
 
 def main():
-    src_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 
-                                                          os.pardir))
-    sys.path.append(src_dir)
-    GAtor()
+    # Check conf file, fill initial pool, then spawn
+    # parallel GA replicas
+    gator = GAtor()
+    gator.initialize_MPI_communicator()
+    gator.check_conf_file()
+    gator.fill_initial_pool()
+    gator.spawn_ga_replicas_mpi4py()
 
 class GAtor():
     """
@@ -46,102 +50,115 @@ class GAtor():
     def __init__(self):
         sname = "GAtor_master"
         self.ui = user_input.get_config()
+        self.src_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),          
+                                                          os.pardir))          
+        sys.path.append(self.src_dir)
         if self.ui.has_option(sname,"working_directory"):
             os.chdir(self.ui.get(sname,"working_directory"))
-            reload_modules()
+            self.reload_modules()
         if self.ui.get_boolean(sname,"testing_mode"):
             self.testing_mode()
             return 
-        if self.ui.get_boolean(sname,"check_conf_file") and\
-            self.ui.is_master_process():
-                self.check_conf_file()
-        if self.ui.get_boolean(sname,"fill_initial_pool") and\
-            self.ui.is_master_process():
-                self.check_conf_file()
-                self.fill_initial_pool()
-        if self.ui.get_boolean(sname,"run_ga"):
-            self.check_initial_pool_filled()
-            self.run_ga()
+
+    def initialize_MPI_communicator(self):
+        from mpi4py import MPI
+        self.comm = MPI.COMM_WORLD                                                      
+        size = self.comm.Get_size() 
 
     def check_conf_file(self):
-        CC = check_conf.ConfigurationChecker() 
-        CC.run_checks()
+        """                                                                        
+        Runs ui.conf consistency checks
+        (e.g. make sure paths exist, etc)
+
+        Will be replaced by full test suite                               
+        """ 
+        if self.comm.Get_rank() == 0:
+            CC = check_conf.ConfigurationChecker() 
+            CC.run_checks()
+        self.comm.Barrier()
 
     def fill_initial_pool(self):
-        IP_module = fh.my_import(self.ui.get("modules",
+        """                                                                        
+        Fills the user-definied initial pool
+        of structures into the GAtor database
+        stored in ./structures                                
+        """ 
+        if self.comm.Get_rank() == 0:
+            IP_module = fh.my_import(self.ui.get("modules",
                                  "initial_pool_module"),
                                  package="initial_pool")
-        fh.mkdir_p(fh.out_tmp_dir)
-        IP_module.main()
+            fh.mkdir_p(fh.out_tmp_dir)
+            IP_module.main()
+        self.comm.Barrier()
 
     def check_initial_pool_filled(self):
-        s = "           "
-        IP_dat = os.path.join(fh.tmp_dir,"num_IP_structs.dat")   
-        msg = "Initial pool has not been filled properly.\n" + s
-        msg += "Make sure [GAtor_master]/fill_initial_pool=TRUE has been run.\n" +s
-        msg += "Check [initial_pool]/stored_energy_name is correct if not 'energy'."
-        if not os.path.isfile(IP_dat):
-             raise Exception(msg)
-        if os.path.isfile(IP_dat):
-            if os.stat(IP_dat).st_size == 0:
-                raise Exception(msg)
+        """
+        Checks the user-defined initial pool
+        has been properly filled into the GAtor 
+        database
+        """
+        if self.comm.Get_rank() == 0:   
+            s = "           "
+            IP_dat = os.path.join(fh.tmp_dir,"num_IP_structs.dat")   
+            msg = "Initial pool has not been filled properly.\n" + s
+            msg += "Check [GAtor_master]/fill_initial_pool=TRUE has been run.\n" +s
+            msg += "Check [initial_pool]/stored_energy_name is correct if not 'energy'."
+            if not os.path.isfile(IP_dat):
+                 raise Exception(msg)
+            if os.path.isfile(IP_dat):
+                if os.stat(IP_dat).st_size == 0:
+                    raise Exception(msg)
+        self.comm.Barrier()
 
-    def run_ga(self):
-        from core import run_GA
-        sname = "parallel_settings"
-        time.sleep(5)
-        if self.ui.get(sname,"parallelization_method") != "serial":
-            #Launch parallelism
-            parallel_run.launch_parallel()
-            return
-        if self.ui.get_replica_name() == "master":
-            #Need to assign new name
-            self.ui.set(sname,"replica_name",misc.get_random_index())
-            conf_path = os.path.join(fh.conf_tmp_dir,
-                        self.ui.get_replica_name()+".conf")
-            f = open(conf_path,"w")
-            self.ui.write(f)
-            f.close()
-            sys.argv.append(conf_path)
-            reload_modules()
-        message = "GAtor instance reporting from " + \
-                                socket.gethostname()
-        if self.ui.has_option(sname,"allocated_nodes"):
-            nodes = self.ui.get_eval(sname,"allocated_nodes")
-            message += "; controlling node(s): " + \
-					", ".join(map(str,nodes))
-        if self.ui.has_option(sname,"processes_per_replica"):
-            ppr = self.ui.get_eval(sname, "processes_per_replica")
-            message += "; controlling %i process(es)" % ppr
-        if self.ui.has_option(sname,"runjob_block"):
-            message += "; block: " + self.ui.get(sname,"runjob_block")
-        if self.ui.has_option(sname,"runjob_corner"):
-            message += "; corner: " + self.ui.get(sname,"runjob_corner")
+    def spawn_ga_replicas_mpi4py(self):
+        """                                                                        
+        Runs parallel GA replicas using mpi4py
+        
+        The core modules such as selection, mutation, and crossover    
+        are run using the RunGA class from /src/core/run_GA_mpi4py  
 
-        output.time_log(message)
-        stoic = stoic_model.determine_stoic()
-        ga = run_GA.RunGA(self.ui.get_replica_name(),stoic)
+        The RunGA class inputs the random                                      
+        """  
+        from core import run_GA_mpi
+        if self.comm.Get_rank() == 0:
+            self.check_initial_pool_filled()
+            stoic = stoic_model.determine_stoic()
+            replica_name = misc.get_random_index()
+            sname = "parallel_settings"
+            self.ui.set(sname,"replica_name", replica_name)       
+            if not os.path.isdir(fh.conf_tmp_dir):
+                os.mkdir(fh.conf_tmp_dir)   
+            conf_path = os.path.join(fh.conf_tmp_dir,                          
+            self.ui.get_replica_name()+".conf")                    
+            f = open(conf_path,"w")                                            
+            self.ui.write(f)                                                   
+            f.close()                                                          
+            sys.argv.append(conf_path)                                         
+            self.reload_modules()  
+        ga = run_GA_mpi.RunGA(replica_name, stoic, self.comm)
         ga.run()
         output.move_to_shared_output(self.ui.get_replica_name())
 	
     def testing_mode(self):
+        """
+        Can be used for developing test and debug features
+        """
         from utilities import test_and_debug
         test_procedure = self.ui.get("test_and_debug","testing_procedure")
-        if self.ui.is_master_process():
-            output.time_log("Testing and debugging mode enabled")
-            output.time_log("Testing procedure used: "+test_procedure)
+        output.time_log("Testing and debugging mode enabled")
+        output.time_log("Testing procedure used: "+test_procedure)
         getattr(test_and_debug,test_procedure)()
         return
-def reload_modules():
-	'''
-	These modules need to be reloaded after the configuration file is changed
-	'''
-	imp.reload(fh)
-	imp.reload(user_input)
-	imp.reload(output)
-	imp.reload(stoic_model)
-	imp.reload(misc)
-	imp.reload(kill)
+
+    def reload_modules(self):
+        '''
+        These modules need to be reloaded if the configuration file is changed
+        '''
+        imp.reload(fh)
+        imp.reload(user_input)
+        imp.reload(output)
+        imp.reload(stoic_model)
+        imp.reload(misc)
 
 if __name__ == "__main__":
     main()
